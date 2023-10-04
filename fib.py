890b6cf423f77684a844620ae2b7d1afc876bcff
stein <mabdulhaseebq@gmail.com>
2023-10-04 17:17:31 +0500

    fib with memoization

---
+++ fib.py
@@ -0,0 +1,28 @@
+# normal implementation
+def fib_normal(n):
+    if n <= 2:
+        return 1
+
+    return fib_normal(n-1) + fib_normal(n-2)
+
+
+# print(fib_normal(6))
+# print(fib_normal(7))
+# print(fib_normal(8))
+
+
+# memoization
+def fib(n, memo={}):
+    
+    # see if we can reuse something
+    if n in memo:
+        # print(memo)
+        return memo[n]
+    
+    if n<=2:
+        return 1
+    
+    memo[n] = fib(n-1, memo) + fib(n-2, memo)
+    return memo[n]
+
+print(fib(15))
