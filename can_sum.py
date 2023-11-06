f905ea4aa8451557f01380f3d086394fde025eda
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-11-06 21:29:55 +0500

    comments added

--- can_sum.py
+++ can_sum.py
@@ -16,8 +16,12 @@ def can_sum(target_sum, numbers, memo={}):
 
 if __name__ == "__main__":
 
-    print(can_sum(7, [2, 3])) # True
-    print(can_sum(7, [5, 3, 4, 7])) # True
+    # print(can_sum(7, [2, 3])) # True
+    # print(can_sum(7, [5, 3, 4, 7])) # True
     print(can_sum(7, [2, 4])) # False
-    print(can_sum(8, [2, 3, 5])) # True
-    print(can_sum(300, [7, 14])) # False (takes too long)
+    # print(can_sum(8, [2, 3, 5])) # True
+    # print(can_sum(300, [7, 14])) # False (takes too long)
+
+"""
+calling the function multiple times returns all true values
+"""
